41eefe8f00b48531e0170cf94649be822ff9977c
marijannovak <marijannovak123@gmail.com>
2017-06-14 09:56:50 +0200

    v1.002

--- android/src/com/marijannovak/littletanks/GameOver.java
+++ android/src/com/marijannovak/littletanks/GameOver.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
